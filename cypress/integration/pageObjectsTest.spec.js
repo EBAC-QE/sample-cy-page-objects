e994dc780a7600e1fe54ba9cbdee467cb11db8e6
Ernesto Barbosa <jose.barbosa@cwi.com.br>
2021-11-08 18:42:38 -0300

    page objects

---
+++ cypress/integration/pageObjectsTest.spec.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+
+const data = require("../fixtures/data.json")
+// const loginPage = require('../support/pages/login.page')
+// const { dashboardPage } = require('../support/pages/dashboard.page')
+const {dashboardPage, loginPage} = require('../support/pages')
+
+describe('Access Admin Panel', () => {
+  beforeEach(() => {
+    cy.visit('/wp-admin')
+  })
+
+  it('should login with valid credentials', () => {
+    loginPage.login(data.usuario, data.senha)
+    dashboardPage.siteName.should("be.visible")
+  })
+})
